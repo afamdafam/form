eb0840cf48d6c4afb8e8c5108726645f56f8d63e
AfamDafam <adam.hp31@gmail.com>
2023-10-09 18:55:30 +0700

    update for route

--- routes/web.php
+++ routes/web.php
@@ -25,29 +25,29 @@ Route::get('forms/create-step-two-1', [FormController::class,'createStepTwo1'])-
 
 Route::post('forms/create-step-two-1', [FormController::class,'postCreateStepTwo1'])->name('forms.create.step.two.post.1');
 
-Route::get('forms/create-step-two-2', [FormController::class,'createStepTwo2'])->name('forms.create.step.two.2');
+Route::get('/create-step-two-2', [FormController::class,'createStepTwo2'])->name('forms.create.step.two.2');
 
-Route::post('forms/create-step-two-2', [FormController::class,'postCreateStepTwo2'])->name('forms.create.step.two.post.2');
+Route::post('/create-step-two-2', [FormController::class,'postCreateStepTwo2'])->name('forms.create.step.two.post.2');
 
-Route::get('forms/create-step-three', [FormController::class,'createStepThree'])->name('forms.create.step.three');
+Route::get('/create-step-three', [FormController::class,'createStepThree'])->name('forms.create.step.three');
 
-Route::post('forms/create-step-three', [FormController::class,'postCreateStepThree'])->name('forms.create.step.three.post');
+Route::post('/create-step-three', [FormController::class,'postCreateStepThree'])->name('forms.create.step.three.post');
 
-Route::get('forms/create-step-four-A', [FormController::class,'createStepFourA'])->name('forms.create.step.four.a');
+Route::get('/create-step-four-A', [FormController::class,'createStepFourA'])->name('forms.create.step.four.a');
 
-Route::post('forms/create-step-four-A', [FormController::class,'postCreateStepFourA'])->name('forms.create.step.four.a.post');
+Route::post('/create-step-four-A', [FormController::class,'postCreateStepFourA'])->name('forms.create.step.four.a.post');
 
-Route::get('forms/create-step-four-B1', [FormController::class,'createStepFourB1'])->name('forms.create.step.four.b1');
+Route::get('/create-step-four-B1', [FormController::class,'createStepFourB1'])->name('forms.create.step.four.b1');
 
-Route::post('forms/create-step-four-B1', [FormController::class,'postCreateStepFourB1'])->name('forms.create.step.four.b1.post');
+Route::post('/create-step-four-B1', [FormController::class,'postCreateStepFourB1'])->name('forms.create.step.four.b1.post');
 
-Route::get('forms/create-step-four-B2', [FormController::class,'createStepFourB2'])->name('forms.create.step.four.b2');
+Route::get('/create-step-four-B2', [FormController::class,'createStepFourB2'])->name('forms.create.step.four.b2');
 
-Route::post('forms/create-step-four-B2', [FormController::class,'postCreateStepFourB2'])->name('forms.create.step.four.b2.post');
+Route::post('/create-step-four-B2', [FormController::class,'postCreateStepFourB2'])->name('forms.create.step.four.b2.post');
 
-Route::get('forms/create-step-four-C', [FormController::class,'createStepFourC'])->name('forms.create.step.four.c');
+Route::get('/create-step-four-C', [FormController::class,'createStepFourC'])->name('forms.create.step.four.c');
 
-Route::post('forms/create-step-four-C', [FormController::class,'postCreateStepFourC'])->name('forms.create.step.four.c.post');
+Route::post('/create-step-four-C', [FormController::class,'postCreateStepFourC'])->name('forms.create.step.four.c.post');
 
-Route::get('forms/forms-success', [FormController::class,'formSuccess'])->name('forms.success');
+Route::get('/forms-success', [FormController::class,'formSuccess'])->name('forms.success');
 
